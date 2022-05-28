98c309fa7771b17e4435609df6ee4fd3069b2930
naseeb-shah <102802675+naseeb-shah@users.noreply.github.com>
2022-05-28 16:50:24 +0530

    ddne

--- index.js
+++ index.js
@@ -6,10 +6,40 @@ function getlist(){
 }
 getlist()
 function display(x){
-    x.forEach(element => {
+    x.forEach((element) => {
 li=document.createElement("li")
-li.innerHTML=element.task
+li.innerHTML=`${element.task}`
+ button=document.createElement('button')
+ button.innerHTML=`<i class="fa-solid fa-trash-can"></i>`
+ button.addEventListener('click',()=>{
+     console.log(element.id)
+    var sai=fetch(`http://localhost:3000/task/${element.id}`,{
+        method: "DELETE",
+                  
+                  headers: {
+                      'Content-Type': "application/json"
+                  }
+      })
+getlist()
+ })
+ li.append(button)
 list.append(li)
         
     });
+}
+function add(){
+    var task=document.getElementById('tasks').value
+    x={
+        "task":task,
+        "status":"false"
+    }
+        fetch('http://localhost:3000/task',{
+          method: "POST",
+                    body: JSON.stringify(x),
+                    headers: {
+                        'Content-Type': "application/json"
+                    }
+        })
+
+    getlist()
 }
